f636caad0769aeda3e6ec02a8cb4cb2de64b732c
Nattapong <inblacksri832@gmail.com>
2019-02-20 11:12:53 +0700

    Edit Calculator 20/02/2019

--- app/src/main/java/sdu/cs/nattapong/calculatorapp/MainActivity.java
+++ app/src/main/java/sdu/cs/nattapong/calculatorapp/MainActivity.java
@@ -16,7 +16,10 @@ public class MainActivity extends AppCompatActivity {
 
         final EditText number1 = findViewById(R.id.edtNumber1);
         final EditText number2 = findViewById(R.id.edtNumber2);
-        Button plus = findViewById(R.id.btPlus);
+        Button plus = findViewById(R.id.bt1);
+        Button minus = findViewById(R.id.bt2);
+        Button multiply = findViewById(R.id.bt3);
+        Button divide = findViewById(R.id.bt4);
         final TextView resultTextView = findViewById(R.id.tvResult);
 
         plus.setOnClickListener(new View.OnClickListener() {
@@ -29,5 +32,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1 = Integer.parseInt(number1.getText().toString());
+                int num2 = Integer.parseInt(number2.getText().toString());
+                int result = num1 - num2;
+                resultTextView.setText(result + "");
+            }
+        });
+
+        multiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1 = Integer.parseInt(number1.getText().toString());
+                int num2 = Integer.parseInt(number2.getText().toString());
+                int result = num1 * num2;
+                resultTextView.setText(result + "");
+            }
+        });
+
+        divide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1 = Integer.parseInt(number1.getText().toString());
+                int num2 = Integer.parseInt(number2.getText().toString());
+                int result = num1 / num2;
+                resultTextView.setText(result + "");
+            }
+        });
     }//end onCreate
 }//end class
